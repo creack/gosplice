1b81ebfbca56fc2e8c9d1287647bfb0fd93402c5
Guillaume J. Charmes <guillaume.charmes@dotcloud.com>
2013-08-30 13:46:30 -0700

    Initial repo

---
+++ splice.go
@@ -0,0 +1,117 @@
+package splice
+
+import (
+	"errors"
+	"io"
+	"os"
+	"sync"
+	"syscall"
+)
+
+const (
+	SPLICE_F_MORE = 0x01
+	SPLICE_F_MOVE = 0x03
+)
+
+var (
+	ErrNoFD error = errors.New("The requested stream does not have Fd() method")
+)
+
+var (
+	pipeFct   = syscall.Pipe
+	spliceFct = syscall.Splice
+)
+
+type Splice struct {
+	sync.Mutex
+	pipe       []int
+	bufferSize int
+	flags      int
+}
+
+type Filer interface {
+	File() (*os.File, error)
+}
+
+type FDer interface {
+	Fd() uintptr
+}
+
+func (s *Splice) SetBufferSize(size int) {
+	s.bufferSize = size
+}
+
+func (s *Splice) SetFlags(flags int) {
+	s.flags = flags
+}
+
+func (s *Splice) Copy(dst io.Writer, src io.Reader) (n int64, err error) {
+	s.Lock()
+	defer s.Unlock()
+
+	var (
+		written int64
+		srcFd   int
+		dstFd   int
+	)
+
+	if f, ok := src.(FDer); !ok {
+		if f, ok := src.(Filer); !ok {
+			return -1, ErrNoFD
+		} else {
+			if f, err := f.File(); err != nil {
+				return -1, err
+			} else {
+				srcFd = int(f.Fd())
+			}
+		}
+	} else {
+		srcFd = int(f.Fd())
+	}
+
+	if t, ok := dst.(FDer); !ok {
+		if t, ok := dst.(Filer); !ok {
+			return -1, ErrNoFD
+		} else {
+			if t, err := t.File(); err != nil {
+				return -1, err
+			} else {
+				dstFd = int(t.Fd())
+			}
+		}
+	} else {
+		dstFd = int(t.Fd())
+	}
+
+	for {
+		w, err := spliceFct(srcFd, nil, s.pipe[1], nil, s.bufferSize, s.flags)
+		if err != nil {
+			return written, err
+		}
+		w, err = spliceFct(s.pipe[0], nil, dstFd, nil, int(w), s.flags)
+		if err != nil {
+			return written + w, err
+		}
+		written += w
+	}
+}
+
+func Copy(dst io.Writer, src io.Reader) (n int64, err error) {
+	s, err := NewSplice()
+	if err != nil {
+		return -1, err
+	}
+	return s.Copy(dst, src)
+}
+
+func NewSplice() (*Splice, error) {
+	pipe := []int{0, 0}
+	if err := pipeFct(pipe); err != nil {
+		return nil, err
+	}
+	return &Splice{
+		pipe:       pipe,
+		bufferSize: 32 * 1024,
+		flags:      SPLICE_F_MOVE | SPLICE_F_MORE,
+	}, nil
+}
---
+++ splice_test.go
@@ -0,0 +1,296 @@
+package splice
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"syscall"
+	"testing"
+)
+
+var listenSpliceFct = listenSplice
+
+func listenPong(port int, ready chan bool) error {
+	l, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", port))
+	if err != nil {
+		return err
+	}
+	defer l.Close()
+	ready <- true
+	rw, err := l.Accept()
+	if err != nil {
+		return err
+	}
+	if _, err := io.Copy(rw, rw); err != nil {
+		if err == io.EOF {
+			return nil
+		}
+		return err
+	}
+	return nil
+}
+
+func listenSplice(listenPort, dstPort int, ready chan bool) error {
+	l, err := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", listenPort))
+	if err != nil {
+		return err
+	}
+	defer l.Close()
+	ready <- true
+
+	rw, err := l.Accept()
+	if err != nil {
+		return err
+	}
+	backend, err := net.Dial("tcp", fmt.Sprintf("127.0.0.1:%d", dstPort))
+	if err != nil {
+		rw.Close()
+		return err
+	}
+	ret := make(chan error, 2)
+	go func() {
+		defer rw.Close()
+		if _, err := Copy(rw, backend); err != nil {
+			if err == io.EOF {
+				ret <- nil
+				return
+			}
+			ret <- err
+		}
+	}()
+	go func() {
+		defer backend.Close()
+		defer rw.Close()
+		if _, err := Copy(backend, rw); err != nil {
+			if err == io.EOF {
+				ret <- nil
+				return
+			}
+			ret <- err
+		}
+	}()
+	err1 := <-ret
+	err2 := <-ret
+	if err1 != nil && err2 != nil {
+		return fmt.Errorf("Error copy1: %s, Error copy2: %s", err1, err2)
+	} else if err1 != nil {
+		return err1
+	} else if err2 != nil {
+		return err2
+	}
+	return nil
+}
+
+func spliceFctFail(rfd int, roff *int64, wfd int, woff *int64, len int, flags int) (n int64, err error) {
+	return -1, fmt.Errorf("Fail")
+}
+
+var spliceFctCount = 0
+
+func spliceFctFail2(rfd int, roff *int64, wfd int, woff *int64, len int, flags int) (n int64, err error) {
+	if spliceFctCount == 1 {
+		spliceFctCount = 0
+		return -1, fmt.Errorf("Fail")
+	}
+	spliceFctCount++
+	return syscall.Splice(rfd, roff, wfd, woff, len, flags)
+}
+
+func pipeFctFail(fds []int) error {
+	return fmt.Errorf("Fail")
+}
+
+func TestNewSplice(t *testing.T) {
+	s, err := NewSplice()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if s == nil {
+		t.Fatal("Failed to instanciate Splice struct")
+	}
+
+	pipeFct = pipeFctFail
+	if _, err := NewSplice(); err == nil {
+		t.Fatal("Pipe excpected to fail but didn't")
+	}
+	pipeFct = syscall.Pipe
+}
+
+func TestSetFlags(t *testing.T) {
+	s, err := NewSplice()
+	if err != nil {
+		t.Fatal(err)
+	}
+	s.SetFlags(SPLICE_F_MORE)
+	if s.flags != SPLICE_F_MORE {
+		t.Fatalf("SetFlags failed, expected flag: %d, found: %d", SPLICE_F_MORE, s.flags)
+	}
+	s.SetFlags(SPLICE_F_MORE | SPLICE_F_MOVE)
+	if s.flags != SPLICE_F_MORE|SPLICE_F_MOVE {
+		t.Fatalf("SetFlags failed, expected flag: %d, found: %d", SPLICE_F_MORE|SPLICE_F_MOVE, s.flags)
+	}
+	s.SetFlags(0)
+	if s.flags != 0 {
+		t.Fatalf("SetFlags failed, expected flag: %d, found: %d", 0, s.flags)
+	}
+}
+
+func TestSetBufferSize(t *testing.T) {
+	s, err := NewSplice()
+	if err != nil {
+		t.Fatal(err)
+	}
+	s.SetBufferSize(32 * 1024)
+	if s.bufferSize != 32*1024 {
+		t.Fatalf("SetBufferSize failed, expected size: %d, found: %d", 32*1024, s.bufferSize)
+	}
+	s.SetBufferSize(0)
+	if s.bufferSize != 0 {
+		t.Fatalf("SetBufferSize failed, expected size: %d, found: %d", 0, s.bufferSize)
+	}
+}
+
+func testCopy(listenPort, dstPort int) error {
+	ready := make(chan bool, 2)
+	ret := make(chan error, 3)
+	go func() {
+		if err := listenPong(dstPort, ready); err != nil {
+			ret <- err
+			return
+		}
+		ret <- nil
+	}()
+	go func() {
+		if err := listenSpliceFct(listenPort, dstPort, ready); err != nil {
+			ret <- err
+			return
+		}
+		ret <- nil
+	}()
+
+	go func() {
+		client, err := net.Dial("tcp", fmt.Sprintf("127.0.0.1:%d", listenPort))
+		if err != nil {
+			ret <- err
+			return
+		}
+		count := 15
+		input := "hello world!"
+		output := input
+		for i := 0; i < count; i++ {
+			if _, err := client.Write([]byte(input)); err != nil {
+				ret <- err
+				return
+			}
+			buf := make([]byte, len(input))
+			if _, err := client.Read(buf); err != nil {
+				if err != io.EOF {
+					ret <- err
+					return
+				}
+			}
+			if string(buf) != output {
+				ret <- fmt.Errorf("Unexpected output (%d). Expected [%s], received [%s]", i, output, string(buf))
+				return
+			}
+		}
+		ret <- nil
+	}()
+
+	select {
+	case <-ready:
+	case r := <-ret:
+		if r != nil {
+			return r
+		}
+	}
+	select {
+	case <-ready:
+	case r := <-ret:
+		if r != nil {
+			return r
+		}
+	}
+
+	return <-ret
+}
+
+func TestCopy(t *testing.T) {
+	if err := testCopy(54931, 54932); err != nil {
+		t.Fatal(err)
+	}
+	pipeFct = pipeFctFail
+	if err := testCopy(54933, 54934); err == nil {
+		t.Fatal("Excpected Copy to fail but didn't")
+	}
+	pipeFct = syscall.Pipe
+
+	spliceFct = spliceFctFail
+	if err := testCopy(54935, 54936); err == nil {
+		t.Fatal("Excpected Copy to fail but didn't")
+	}
+	spliceFct = syscall.Splice
+
+	// spliceFct = spliceFctFail2
+	// if err := testCopy(54937, 54938); err == nil {
+	// 	t.Fatal("Excpected Copy to fail but didn't")
+	// }
+	// spliceFct = syscall.Splice
+}
+
+func BenchmarkCopy(b *testing.B) {
+	b.Skip("Unimplemented")
+
+	b.StopTimer()
+
+	println("HELLO")
+	dstPort := 54772
+	listenPort := 54773
+
+	ready := make(chan bool, 2)
+	ret := make(chan error, 3)
+	go func() {
+		if err := listenPong(dstPort, ready); err != nil {
+			ret <- err
+			return
+		}
+		ret <- nil
+	}()
+	go func() {
+		if err := listenSpliceFct(listenPort, dstPort, ready); err != nil {
+			ret <- err
+			return
+		}
+		ret <- nil
+	}()
+
+	b.StartTimer()
+	for i := 0; i < 1; i++ {
+		client, err := net.Dial("tcp", fmt.Sprintf("127.0.0.1:%d", listenPort))
+		if err != nil {
+			ret <- err
+			return
+		}
+		count := 15
+		input := "hello world!"
+		output := input
+		for i := 0; i < count; i++ {
+			if _, err := client.Write([]byte(input)); err != nil {
+				ret <- err
+				return
+			}
+			buf := make([]byte, len(input))
+			if _, err := client.Read(buf); err != nil {
+				if err != io.EOF {
+					ret <- err
+					return
+				}
+			}
+			if string(buf) != output {
+				ret <- fmt.Errorf("Unexpected output (%d). Expected [%s], received [%s]", i, output, string(buf))
+				return
+			}
+		}
+		ret <- nil
+	}
+}
